ff1dddb10d408657a5b13918530c68646719dfd0
Piotr Bator <82182989+piotek8@users.noreply.github.com>
2023-04-03 16:17:24 +0200

    Add files via upload

---
+++ Designed for recruitment task.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+url = 'https://techsummer.ringieraxelspringer.com/recruitmentTask'
+
+path = "C:\\Users\\hp\\Downloads\\chromedriver"
+driver = webdriver.Chrome(path)
+
+driver.get(url)
+
+time.sleep(5)
+
+input_element = driver.find_element('xpath',"//input[@id='task-answer']")
+
+def make_commands():
+    input_element.send_keys(Keys.CONTROL, 'a')
+    input_element.send_keys(Keys.BACKSPACE)   
+    input_element.send_keys(str(i))
+    time.sleep(1) # czekaj 1 sekundę
+    submit_button = driver.find_element('xpath', "//div[@class='task-buttton submit']")
+    submit_button.click()
+    time.sleep(1) # czekaj 1 sekundę
+    close_button = driver.find_element('xpath', "//span[@class='close-button']")
+    close_button.click()
+
+    print(i)
+    
+for i in range(1, 1001):
+    make_commands()
+
+driver.quit()
+
+
+# THE SCORE IS 65
